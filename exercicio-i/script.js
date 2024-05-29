88640e682960102d578cc0dcc8872b3ea64405dd
thyagolima23 <thyago.lima2324@gmail.com>
2024-05-28 22:20:52 -0300

    finalizar trabalho

--- exercicio-i/script.js
+++
@@ -1,7 +0,0 @@
-alert("Bem-vindo!");
-let valor1 = parseInt(prompt("informe o valor 1"));
-let valor2 = parseInt(prompt("informe o valor 2"));
-let valor3 = parseInt(prompt("informe o valor 3"));
-let valor4 = parseInt(prompt("informe o valor 4"));
-let valor5= parseInt(prompt("informe o valor 5"));
-if(
